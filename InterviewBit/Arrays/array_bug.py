d0d28ff00870125bd8ac50b784be1416a776e89a
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-11-21 20:43:18 +0700

    InterviewBit - Arrays: Array Bug solution

---
+++ InterviewBit/Arrays/array_bug.py
@@ -0,0 +1,26 @@
+# https://www.interviewbit.com/problems/arraybug
+
+class MySolution:
+  # @param a : list of integers
+  # @param b : integer
+  # @return a list of integers
+  # We need to rotate array b times so loop b times
+  #   Take out first element
+  #   Append back to array
+  def rotateArray(self, a, b):
+    ret = []
+    for i in xrange(b):
+      first = a.pop(0)
+      ret = a
+      ret.append(first)
+    return ret
+
+class IBSolution:
+  # @param a : list of integers
+  # @param b : integer
+  # @return a list of integers
+  def rotateArray(self, a, b):
+    ret = []
+    for i in xrange(len(a)):
+      ret.append(a[(i + b) % len(a)])
+    return ret
